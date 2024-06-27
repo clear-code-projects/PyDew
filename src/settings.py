874c1f9a537fa5b604373e6fd58c14cafcd97882
fortwoone <denauxarthur@gmail.com>
2024-06-27 13:06:05 +0200

    requirements.txt and prevent the game from running if Pygame CE is not installed

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,6 @@
 import pygame, sys
+if not getattr(pygame, "IS_CE", False):
+    raise ImportError("The game requires Pygame CE to function. (hint: type pip uninstall pygame and then pip install pygame-ce)")
 from os.path import join
 from os import walk, path, sep, listdir
 from pytmx.util_pygame import load_pygame
