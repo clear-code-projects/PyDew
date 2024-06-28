cf20cb8efd9761bb93dc4630be31827e6be6f95b
fortwoone <denauxarthur@gmail.com>
2024-06-28 11:18:18 +0200

    The game now warns the user if Python is not 3.12 or above

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,9 @@
-import pygame, sys
+import pygame, sys, warnings
 if not getattr(pygame, "IS_CE", False):
     raise ImportError("The game requires Pygame CE to function. (hint: type pip uninstall pygame and then pip install pygame-ce)")
+if sys.version_info < (3, 12):
+    warnings.warn(f"The project is currently running under Python {sys.version_info.major}.{sys.version_info.minor}. Consider upgrading to 3.12 or the most recent version available before running the game further.",
+                  DeprecationWarning)
 from os.path import join
 from os import walk, path, sep, listdir
 from pytmx.util_pygame import load_pygame
